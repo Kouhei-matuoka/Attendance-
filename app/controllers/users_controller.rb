7f4af56a300afaa6a5beef30a65ab8b90bfdd303
Kouhei-matuoka <kouhei19951112@gmail.com>
2020-05-30 08:25:05 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
@@ -19,6 +20,7 @@ class UsersController < ApplicationController
   end
   
   private
+  
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
